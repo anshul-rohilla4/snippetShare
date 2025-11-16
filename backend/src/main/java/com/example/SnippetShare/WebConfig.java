7d297cf59d8250ba9a73930d2036f3e07caccef6
anshul-rohilla4 <anshulrohilla111@gmail.com>
2025-11-16 18:24:25 +0530

    frontend access from web link

--- backend/src/main/java/com/example/SnippetShare/WebConfig.java
+++ backend/src/main/java/com/example/SnippetShare/WebConfig.java
@@ -15,7 +15,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://snippetsshare.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
