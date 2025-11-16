f3282d09a55145ce4236a6b4f4b3a3ca14ad0a36
anshul-rohilla4 <anshulrohilla111@gmail.com>
2025-11-16 18:08:01 +0530

    backend link modified

--- frontend/src/services/SnippetService.jsx
+++ frontend/src/services/SnippetService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/snippets';
+const API_BASE_URL = 'https://snippetshare-s4xb.onrender.com/api/snippets';
 
 const getAllSnippets = () => {
     return axios.get(`${API_BASE_URL}/browse`);
